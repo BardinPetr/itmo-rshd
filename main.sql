58334c6465d845775f86914717c5033fcbe99f09
Bardin Petr <bardin.petr@gmail.com>
2025-02-28 15:44:53 +0300

    LAB 1

--- main.sql
+++ main.sql
@@ -42,6 +42,7 @@ $$
         target_table_schema TEXT = 'public';
         target_table_name   TEXT = 'Н_УЧЕНИКИ'; -- 'warehouse_products';
         target_table_oid    OID;
+        row_cnt             INT;
         header_fmt          TEXT = E'%-3s %-20s| %-30s| %-60s| %-20s| %s\n';
         data_fmt            TEXT;
         output              TEXT = '';
@@ -52,10 +53,11 @@ $$
         data_fmt := header_fmt || E'Среднее: %s\n \n';
 
         -- fetch table oid
-        SELECT cls.oid
-        INTO target_table_oid
+        SELECT cls.oid, stat.n_live_tup
+        INTO target_table_oid, row_cnt
         FROM pg_class cls
                  LEFT JOIN pg_namespace ns ON ns.oid = cls.relnamespace
+                 LEFT JOIN pg_stat_user_tables stat ON stat.relid = cls.oid
         WHERE ns.nspname = target_table_schema
           AND cls.relname = target_table_name;
 
@@ -83,7 +85,8 @@ $$
                    CASE
                        WHEN fk_dst_attr.attname IS NOT NULL
                            THEN format('FOREIGN(%s.%s)', fk_dst_table.relname, fk_dst_attr.attname)
-                       ELSE '' END                                                          AS is_foreign
+                       ELSE '' END                                                          AS is_foreign,
+                   stat.n_distinct                                                          as c_distinct
             FROM pg_attribute att
                      LEFT JOIN pg_description d -- for comments
                                ON d.objsubid = att.attnum AND d.objoid = target_table_oid
@@ -96,16 +99,17 @@ $$
                                    AND fk_dst_attr.attnum = fk_constr.confkey[1]
                      LEFT JOIN pg_class fk_dst_table
                                ON fk_dst_table.oid = fk_constr.confrelid
+                     LEFT JOIN pg_stats stat
+                               ON stat.schemaname = target_table_schema
+                                   AND stat.tablename = target_table_name
+                                   AND stat.attname = att.attname
             WHERE att.attnum > 0
-              AND att.attrelid = target_table_oid
+              AND att.
+                      attrelid = target_table_oid
               AND NOT att.attisdropped
             ORDER BY ord
             LOOP
-                -- fetch distinct count
-                EXECUTE format('SELECT COUNT(*) FROM (SELECT DISTINCT %I FROM %I.%I) AS subquery',
-                               col.name,
-                               target_table_schema,
-                               target_table_name) INTO tmp_distinct;
+                tmp_distinct := CASE WHEN col.c_distinct < 0 THEN col.c_distinct * row_cnt ELSE col.c_distinct END;
 
                 output := output ||
                           format(data_fmt,
@@ -130,4 +134,3 @@ $$
             output;
     END
 $$;
-
